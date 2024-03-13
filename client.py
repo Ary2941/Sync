83b0176929ee18ceaa21f482c7c761a94952dfff
Ary2941 <amaury2941@gmail.com>
2024-03-13 20:41:23 -0300

    deixando mais legível

--- client.py
+++ client.py
@@ -2,22 +2,22 @@ from xmlrpc.client import ServerProxy
 from datetime import datetime
 import asyncio
 
+
 # Configurações do cliente
 SERVER_ADDRESS = "http://localhost:8000"
 UPDATE_INTERVAL = 5  # Intervalo de atualização em segundos
 
-async def update_time():
+async def update_time(server):
     while True:
         # Obtém o tempo do cliente
         client_time = datetime.now().timestamp()
-
-        # Envia o tempo ao servidor e recebe a diferença de tempo
-        server.calculate_time_difference(client_time)
-
-        # Ajusta o tempo no cliente com base na diferença de tempo recebida do servidor
-        adjusted_time = server.adjust_time()
+        print("enviando tempo para o server...")
+        server.request_client_time()
+        time_difference = server.add_client_time(client_time)
+        adjusted_time = server.calculate_average_time()
 
         print(f"Tempo do cliente: {client_time}")
+        print(f"Diferença de tempo recebida do servidor: {time_difference}")
         print(f"Tempo ajustado no cliente: {adjusted_time}")
         print("----")
 
@@ -28,4 +28,4 @@ async def update_time():
 server = ServerProxy(SERVER_ADDRESS, allow_none=True)
 
 # Inicia o loop de atualização assíncrono
-asyncio.run(update_time())
+asyncio.run(update_time(server))
--- server.py
+++ server.py
@@ -2,32 +2,44 @@ from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from datetime import datetime
 
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+
+
 # Configurações do servidor
 HOST = "localhost"
 PORT = 8000
 
-# Variável para armazenar a diferença de tempo
-time_difference = 0
-
 # Classe do servidor
 class BerkeleyServer:
+    def __init__(self):
+        self.client_times = []
+
     def get_time(self):
         return datetime.now().timestamp()
 
-    def calculate_time_difference(self, client_time):
-        server_time = datetime.now().timestamp()
-        global time_difference
-        time_difference = server_time - client_time
-        return time_difference
+    def add_client_time(self, client_time):
+        self.client_times.append(client_time)
+        print(f"Tempo do cliente adicionado: {client_time}")
+        return True
 
-    def adjust_time(self):
-        current_time = datetime.now().timestamp()
-        adjusted_time = current_time + time_difference
-        return adjusted_time
+    def calculate_average_time(self):
+        server_time = datetime.now().timestamp()
+        all_times = [server_time] + self.client_times
+        average_time = sum(all_times) / len(all_times)
+        return average_time
+
+    def request_client_time(self):
+        return True
 
 # Configuração do servidor RPC
 server = SimpleXMLRPCServer((HOST, PORT), requestHandler=SimpleXMLRPCRequestHandler, allow_none=True)
 server.register_instance(BerkeleyServer())
 
 print(f"Servidor Berkeley RPC rodando em {HOST}:{PORT}")
-server.serve_forever()
+
+# Aguarda a conexão do cliente e solicita seu tempo
+while True:
+    server.handle_request()
